9d8383e362589acced7fe94fb1f9066ff996d73a
einkelflugle <cluckyferret@gmail.com>
2014-09-03 16:31:09 +1000

    Added tier filter when viewing robots

--- app/controllers/tiers_controller.rb
+++ app/controllers/tiers_controller.rb
@@ -1,4 +1,8 @@
 class TiersController < ApplicationController
+	def index
+		@tiers = Tier.all
+	end
+
 	def show
 		@tier = Tier.find_by(name: params[:name])
 		@robots = Robot.where(tier_id: @tier.id)
