88e79907a3f13f3b2fa2d777b7b27c6d3e900e58
einkelflugle <cluckyferret@gmail.com>
2014-09-04 19:54:41 +1000

    Added validations to competitions

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,7 @@
 class Competition < ActiveRecord::Base
 	has_many :entries, dependent: :destroy
 	has_many :robots, through: :entries
+
+	validates :name, presence: true, length: { minimum: 5, maximum: 30 }
+	validates :description, presence: true, length: { minimum: 5 }
 end
