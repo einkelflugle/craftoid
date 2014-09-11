f672b7760f422f97c105f9daf011b7f5d1d8c33b
einkelflugle <cluckyferret@gmail.com>
2014-09-11 18:07:46 +1000

    Increased limit for competition names

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -4,7 +4,7 @@ class Competition < ActiveRecord::Base
 	has_and_belongs_to_many :categories
 	belongs_to :user
 
-	validates :name, presence: true, length: { minimum: 5, maximum: 30 }
+	validates :name, presence: true, length: { minimum: 5, maximum: 50 }
 	validates :description, presence: true, length: { minimum: 5, maximum: 2500 }
 
 	include Viewable
