b5dd5433627633732aa817be4ac5216cf630b85a
einkelflugle <cluckyferret@gmail.com>
2014-09-13 12:13:03 +1000

    Made tooltips more visible on default backgrounds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,8 @@ end
 Category.create(name: "Sniper", description: "A lightly armoured robot with powerful, long-rage railguns.")
 Category.create(name: "Gunbed", description: "A very well armoured wheeled vehicle, normally equipped with large amounts of lasers.")
 Category.create(name: "Flyer", description: "A moderately armoured winged robot, usually with several thrusters. Weaponry can vary.")
+
+# Create the three Robocraft weapon types
+Weapon.create(name: "Subatomic Machine Gun", short_name: "smg")
+Weapon.create(name: "Plasma Launcher", short_name: "plasma")
+Weapon.create(name: "Rail Cannon", short_name: "rail")
