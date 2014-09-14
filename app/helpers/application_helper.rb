a8b742a324a247760ac71b867b3e777ffc23cace
einkelflugle <cluckyferret@gmail.com>
2014-09-14 14:07:54 +1000

    Extracted linked text method into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	# Nicely formats text with line breaks and add links
+	def linked_text_body(text, options = {})
+		if options[:truncate]
+			text = truncate(text, length: options[:truncate], separator: ' ')
+		end
+		auto_link(h(text).gsub(/\n/, '<br>').html_safe, html: { target: '_blank' })
+	end
 end
