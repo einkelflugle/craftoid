8fcc009e8516b7222159b25b0cfca67d228d0139
einkelflugle <cluckyferret@gmail.com>
2014-09-29 15:23:01 +1000

    Increased maximum comment length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,6 @@ class Comment < ActiveRecord::Base
 	belongs_to :user
 	validates_presence_of :robot, :body
 
-	validates :body, length: { minimum: 5, maximum: 60 }
+	validates :body, length: { minimum: 5, maximum: 300 }
+
 end
